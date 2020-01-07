ce035f8fff78bfb891651aa72ddb55ba743fc7f8
angelflower <angelfc.contact@gmail.com>
2020-01-07 14:08:56 -0600

    Cambios

--- src/componentes/Rutas.js
+++ src/componentes/Rutas.js
@@ -5,13 +5,13 @@ import Inicio from '../paginas/Inicio';
 import Proyectos from '../paginas/Proyectos';
 import Contribuir from '../paginas/Contribuir';
 
-
 const Rutas = () => (
-  <Switch>
-    <Route exact path="/" component={Inicio} />
-    <Route path="/proyectos" component={Proyectos} />
-    <Route path="/contribuir" component={Contribuir} />
-  </Switch>
+      <Switch>
+        <Route exact path="/" component={Inicio} />
+        <Route path="/proyectos" component={Proyectos} />
+        <Route path="/contribuir" component={Contribuir} />
+        <Route component={() => (<div className="flex not-found"><h2>Error 404, página no encontrada</h2> </div>)} />
+      </Switch>
 )
 
 export default Rutas;
